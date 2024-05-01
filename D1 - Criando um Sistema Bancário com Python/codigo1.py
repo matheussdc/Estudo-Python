f932d4cecb66f31a594f73609b0f93e88774fdb2
Matheus Sá de Castro <mscastro30@gmail.com>
2024-05-01 16:21:58 -0300

    Implementado Depósito

--- D1 - Criando um Sistema Bancário com Python/codigo1.py
+++ D1 - Criando um Sistema Bancário com Python/codigo1.py
@@ -10,11 +10,25 @@ def main():
     [q] - Sair
     Selecione uma operação: """
 
+    saldo = 0     #Saldo inicial = 0
+    extrato = []  #Extrato com registro de depósitos e saques por String
+
     while True:
         operation = input(textwrap.dedent(menu))
 
         if operation == '1':
-            print("Depósito")
+            print("Depósito - ", end="")
+            while True:
+                try:
+                    valor_depositado = float(input("Insira a quantidade a ser depositada: R$"))
+                    if valor_depositado > 0:
+                        saldo += valor_depositado
+                        extrato.append(f"Depositados R$ {valor_depositado:.2f}")
+                        break
+                    else:
+                        print("Valor inválido para depósito!! - ", end="")
+                except ValueError:
+                    print("Valor inválido para depósito!! - ", end="")
 
         elif operation == '2':
             print("Saque")
