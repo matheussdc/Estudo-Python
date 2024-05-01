c75c7ecc08c35f9ac35aefeaa79ce09291b73477
Matheus Sá de Castro <mscastro30@gmail.com>
2024-05-01 16:54:18 -0300

    Implementado Saque

--- D1 - Criando um Sistema Bancário com Python/codigo1.py
+++ D1 - Criando um Sistema Bancário com Python/codigo1.py
@@ -10,8 +10,10 @@ def main():
     [q] - Sair
     Selecione uma operação: """
 
-    saldo = 0     #Saldo inicial = 0
-    extrato = []  #Extrato com registro de depósitos e saques por String
+    saldo = 0                   #Saldo inicial = 0
+    extrato_historico = []                #Extrato com registro de depósitos e saques por String
+    saque_limite_valor = 500.00 #Limite de saque diário R$500.00
+    saque_limite_vezes = 3      #No máximo 3 saques por dia
 
     while True:
         operation = input(textwrap.dedent(menu))
@@ -23,7 +25,7 @@ def main():
                     valor_depositado = float(input("Insira a quantidade a ser depositada: R$"))
                     if valor_depositado > 0:
                         saldo += valor_depositado
-                        extrato.append(f"Depositados R$ {valor_depositado:.2f}")
+                        extrato_historico.append(f"Depósito de R$ {valor_depositado:.2f}")
                         break
                     else:
                         print("Valor inválido para depósito!! - ", end="")
@@ -31,7 +33,28 @@ def main():
                     print("Valor inválido para depósito!! - ", end="")
 
         elif operation == '2':
-            print("Saque")
+            if saque_limite_vezes == 0:
+                print("Limite de saque diário atingido !!")
+                continue
+
+            print("Saque - ", end="")
+            while True:
+                try:
+                    valor_sacado = float(input("Insira a quantidade a ser sacada: R$"))
+                    if valor_sacado > saque_limite_valor:
+                        print(f"Valor limite de {saque_limite_valor:.2f} de saque ultrapassado!! - ", end="")
+                    elif valor_sacado > saldo:
+                        print("Não há saldo suficiente para o saque desejado")
+                        break
+                    elif valor_sacado > 0:
+                        saldo -= valor_sacado
+                        extrato_historico.append(f"Saque de R$ {valor_sacado:.2f}")
+                        saque_limite_vezes -= 1
+                        break
+                    else:
+                        print("Valor inválido para saque!! - ", end="")
+                except ValueError:
+                    print("Valor inválido para saque!! - ", end="")
 
         elif operation == '3':
             print("Extrato")
