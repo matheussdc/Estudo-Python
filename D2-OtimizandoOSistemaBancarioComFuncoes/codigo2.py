28bc98726d190bb6ce7a3278bf56f74c00001360
Matheus Sá de Castro <mscastro30@gmail.com>
2024-05-02 20:59:56 -0300

    add deposito + saque + extrato

--- D2-OtimizandoOSistemaBancarioComFuncoes/codigo2.py
+++ D2-OtimizandoOSistemaBancarioComFuncoes/codigo2.py
@@ -1,16 +1,46 @@
 import textwrap
 
 
-def saque():  # Keyword only (*,)
-    return None
+def saque(*, saldo, limite_vezes, limite_valor, extrato_hist):  # Keyword only (*,)
+    if limite_vezes == 0:
+        print("Limite de saque diário atingido !!")
+        return None
+
+    print("Saque - ", end="")
+    while True:
+        try:
+            valor_sacado = float(input("Insira a quantidade a ser sacada: R$"))
+            if valor_sacado > limite_valor:
+                print(f"Valor limite de {limite_valor:.2f} de saque ultrapassado!! - ", end="")
+            elif valor_sacado > saldo:
+                print("Não há saldo suficiente para o saque desejado")
+                break
+            elif valor_sacado > 0:
+                saldo -= valor_sacado
+                extrato_hist.append(f"Saque de R$ {valor_sacado:.2f}")
+                limite_vezes -= 1
+                return saldo, limite_vezes
+            elif valor_sacado == 0:
+                return None
+            else:
+                print("Valor inválido para saque!! - ", end="")
+        except ValueError:
+            print("Valor inválido para saque!! - ", end="")
 
 
-def deposito():  # Positional only (,/)
-    return None
+def deposito(saldo, valor, extrato_hist, /):  # Positional only (,/)
+    saldo += valor
+    extrato_hist.append(f"Depósito de R$ {valor:.2f}")
+    return saldo
 
 
-def extrato(saldo, /, *, extrato):
-    return None
+def extrato(saldo, /, *, extrato_hist):
+    if extrato_hist:
+        for movimentacao in extrato_hist:
+            print(movimentacao)
+    else:
+        print("Não foram realizadas movimentações")
+    print(f"Saldo disponível: R$ {saldo:.2f}")
 
 
 # Nome, data de nascimento, CPF, endereço = "logradouro - nro - bairro - cidade/estado"
@@ -66,13 +96,25 @@ def main():
         operation = input(textwrap.dedent(menu))
 
         if operation == '1':
-            deposito()
+            print("Depósito - ", end="")
+            while True:
+                try:
+                    valor_depositado = float(input("Insira a quantidade a ser depositada: R$"))
+                    if valor_depositado > 0:
+                        saldo = deposito(saldo, valor_depositado, extrato_historico)
+                        break
+                    elif valor_depositado == 0:
+                        break
+                    else:
+                        print("Valor inválido para depósito!! - ", end="")
+                except ValueError:
+                    print("Valor inválido para depósito!! - ", end="")
 
         elif operation == '2':
-            saque()
+            saldo, saque_limite_vezes = saque(saldo=saldo, limite_vezes=saque_limite_vezes, limite_valor=saque_limite_valor, extrato_hist=extrato_historico)
 
         elif operation == '3':
-            extrato(2, extrato=3)
+            extrato(saldo, extrato_hist=extrato_historico)
 
         elif operation == '4':
             novo_usuario = criar_usuario(lista_usuarios)
