1ea94676b5c66643a4ea5b4747a3bbf0c51c8d58
Matheus Sá de Castro <mscastro30@gmail.com>
2024-05-02 21:17:14 -0300

    att lista_usuarios agora é lista e não mais dict

--- D2-OtimizandoOSistemaBancarioComFuncoes/codigo2.py
+++ D2-OtimizandoOSistemaBancarioComFuncoes/codigo2.py
@@ -45,10 +45,11 @@ def extrato(saldo, /, *, extrato_hist):
 
 # Nome, data de nascimento, CPF, endereço = "logradouro - nro - bairro - cidade/estado"
 def criar_usuario(lista_usuarios):
-    cpf = input("Criar Usuário - Insira o CPF do novo usuário: ")  # Sem tratar a validade
-    if cpf in lista_usuarios:
-        print("Usuário já cadastrado!!")
-        return None
+    cpf_novo = input("Criar Usuário - Insira o CPF do novo usuário: ")  # Sem tratar a validade
+    for cpf, user in lista_usuarios:
+        if cpf_novo == cpf:
+            print("Usuário já cadastrado!!")
+            return None
     nome = input("Insira o nome do novo usuário: ")
     data = input("Insira a data de nascimento do novo usuário: ")
     logradouro = input("Insira o logradouro do novo usuário: ")
@@ -57,14 +58,14 @@ def criar_usuario(lista_usuarios):
     cidade = input("Insira a cidade do novo usuário: ")
     estado = input("Insira o estado do novo usuário: ")
     endereco = {"logradouro": logradouro, "numero": numero, "cidade": cidade, "bairro": bairro, "estado": estado}
-    novo_usuario = {cpf: {"nome": nome, "nascimento": data, "endereço": endereco}}
+    novo_usuario = (cpf_novo, {"nome": nome, "nascimento": data, "endereço": endereco})
     return novo_usuario
 
 
 def listar_usuarios(lista_usuarios):
     print("Usuários:")
     if lista_usuarios:
-        for cpf, user in lista_usuarios.items():
+        for cpf, user in lista_usuarios:
             nome, data, endereco = user.values()
             log, num, city, bairro, estado = endereco.values()
             print(f"{cpf}: Nome: {nome}, Nascimento: {data}, Endereço: {log} - {num} - {bairro} - {city}/{estado}")
@@ -72,7 +73,7 @@ def listar_usuarios(lista_usuarios):
         print("Não há usuários cadastrados")
 
 
-def criar_conta_corrente(): # Agência, nro da conta, usuário
+def criar_conta_corrente(lista_usuarios): # Agência, nro da conta, usuário
     return None
 
 
@@ -84,9 +85,12 @@ def main():
     [3] - Extrato
     [4] - Criar Usuário
     [5] - Listar Usuários
+    [6] - Criar Conta Corrente
+    [7] - Listar Contas
     [q] - Sair
     Selecione uma operação: """
-    lista_usuarios = {}
+    lista_usuarios = []
+    lista_contas = []
     saldo = 0                    # Saldo inicial = 0
     extrato_historico = []       # Extrato com registro de depósitos e saques por String
     saque_limite_valor = 500.00  # Limite de saque diário R$500.00
@@ -119,10 +123,15 @@ def main():
         elif operation == '4':
             novo_usuario = criar_usuario(lista_usuarios)
             if novo_usuario is not None:
-                lista_usuarios.update(novo_usuario)
+                lista_usuarios.append(novo_usuario)
 
         elif operation == '5':
             listar_usuarios(lista_usuarios)
+
+        elif operation =='6':
+            nova_conta = criar_conta_corrente(lista_usuarios)
+            if nova_conta is not None:
+                lista_contas.append(nova_conta)
 
         elif operation == 'q':
             print("Saída")
